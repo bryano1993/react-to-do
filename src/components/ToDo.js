138bf7f6a523ac603947a5e39111800dd7c63775
Bryan O'Connor <bryano1993@gmail.com>
2018-04-11 17:40:18 -0700

    assignment-8-Events: complete

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,14 +5,9 @@ class ToDo extends Component {
     return (
       <li>
         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-        <input type="button" value="Delete" />
+        <input type="button" value="Delete" onClick={ this.props.deleteTodo} />
         <span>{ this.props.description }</span>
-
       </li>
-//create a checkbox input type and render the isCompleted prop as the value of the checked property with a type attribute of checkbox.
-// Added an event listener called onChange that will call the anonymous function this.toggleComplete and pass it to the index. We are passing a function as a prop.
-
-//In the toDo component, we access the props frpm the App component by using the this.props object. We render the description prop in our ToDo component.
     );
   }
 }
